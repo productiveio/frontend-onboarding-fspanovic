732ad5e2922346ee4d56ac2c311ceaa616edb02f
Filip Spanovic <filipspanovic@MacBook-Pro-od-Filip.local>
2024-12-05 10:10:17 +0100

    added route guards

--- app/routes/authenticated.ts
+++ app/routes/authenticated.ts
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
+import type Transition from '@ember/routing/transition';
+import { inject, service } from '@ember/service';
 
-export default class AuthenticatedRoute extends Route {}
+export default class AuthenticatedRoute extends Route {
+    @inject('router') router;
+    @service session;
+
+    beforeModel(transition: Transition): Promise<unknown> | void {
+        if(!this.session.isAuthenticated){
+            this.router.transitionTo("/login")
+        }
+    }
+    
+}
