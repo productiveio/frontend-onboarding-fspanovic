9c5d8c0f4f3e2d851cdedcfe49dfdf2f8223450b
Filip Spanovic <filipspanovic@MacBook-Pro-od-Filip.local>
2024-12-05 09:02:20 +0100

    fix: two way binding of form elements

--- app/components/login-component.ts
+++ app/components/login-component.ts
@@ -1,15 +1,25 @@
-import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-
+import { action } from '@ember/object';
+import { service } from '@ember/service';
 
 export default class LoginComponent extends Component {
+  @service session;
   @tracked email= "";
   @tracked password= "";
 
   @action
+  onInputChange(e:InputEvent){
+    const target = e.target as HTMLInputElement
+    this[target.id] = (target  as HTMLInputElement).value
+  }
+
+
+
+  @action
   submitLogin(e:SubmitEvent){
-    e.preventDefault()
+    e.preventDefault();
+    this.session.login(this.email, this.password)
   }
  
 }
---
+++ app/services/session.ts
@@ -0,0 +1,44 @@
+import Service, { inject } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+export default class SessionService extends Service {
+  @tracked currentUser = null;
+  @tracked isAuthenticated = false;
+  @tracked token = null;
+  @inject('router') router;
+
+  get isLoggedin(){
+    return this.isAuthenticated
+  }
+
+  async login(email: string, password: string) {
+    console.log(email, password, 'email and pw')
+   
+    const response = await fetch('https://api-staging.productive.io/api/v2/sessions', {
+      method: 'POST',
+      body: JSON.stringify({ data: {
+        type: "sessions",
+        attributes:{
+          email,
+          password
+        }
+      }  }),
+    });
+
+    // const { user, token } = await response.json();
+    // this.currentUser = user;
+    // this.token = token;
+    // this.isAuthenticated = true;
+    this.router.transitionTo("/")
+  }
+}
+
+// Don't remove this declaration: this is what enables TypeScript to resolve
+// this service using `Owner.lookup('service:session')`, as well
+// as to check when you pass the service name as an argument to the decorator,
+// like `@service('session') declare altName: SessionService;`.
+declare module '@ember/service' {
+  interface Registry {
+    'session': SessionService;
+  }
+}
