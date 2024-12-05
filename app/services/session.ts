995c4c5e371ee13c1d835eb374a9b56eb18f03d5
Filip Spanovic <filipspanovic@MacBook-Pro-od-Filip.local>
2024-12-05 13:27:31 +0100

    login method removed from session service

--- app/services/session.ts
+++ app/services/session.ts
@@ -11,31 +11,6 @@ export default class SessionService extends Service {
   get isLoggedin(){
     return this.isAuthenticated
   }
-
-  async login(email: string, password: string) {
-    console.log(email, password, 'email and pw')
-   
-    const response = await fetch('https://api-staging.productive.io/api/v2/sessions', {
-      method: 'POST',
-      headers: {
-        "Accept": "text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5",
-        "Content-Type": "application/vnd.api+json",
-      },
-      body: JSON.stringify({ data: {
-        type: "sessions",
-        attributes:{
-          email,
-          password,
-        },
-      }  }),
-    });
-
-    // const { user, token } = await response.json();
-    // this.currentUser = user;
-    // this.token = token;
-    // this.isAuthenticated = true;
-    this.router.transitionTo("/")
-  }
 }
 
 // Don't remove this declaration: this is what enables TypeScript to resolve
