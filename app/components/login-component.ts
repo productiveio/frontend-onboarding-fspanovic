0f5c922f6c23537b6f60209a253c06c28e7f3005
Filip Spanovic <filipspanovic@MacBook-Pro-od-Filip.local>
2024-12-05 16:52:37 +0100

    duplicate header removed

--- app/components/login-component.ts
+++ app/components/login-component.ts
@@ -4,9 +4,11 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject, service } from '@ember/service';
-import type StoreService from '@ember-data/store'
-import type SessionService from 'frontend-onboarding-template/services/session';
+
 import type RouterService from '@ember/routing/router-service';
+import type StoreService from '@ember-data/store'
+import type SessionModel from 'frontend-onboarding-template/models/session';
+import type SessionService from 'frontend-onboarding-template/services/session';
 
 export default class LoginComponent extends Component {
   @service declare session:SessionService;
@@ -18,24 +20,23 @@ export default class LoginComponent extends Component {
   @action
   onInputChange(event:Event){
     const target = event.target as HTMLInputElement
+
     this[target.id] = (target).value
   }
 
   @action
-  submitLogin(e:SubmitEvent){
+  async submitLogin(e:SubmitEvent){
     e.preventDefault();
-    try{
-      const session = this.store.createRecord("session", {email:this.email, password:this.password});
-      session.save().then((res:any)=>{
-        localStorage.setItem("token", res?.token) 
+
+      try{
+        const session = await this.store.createRecord("session", {email:this.email, password:this.password});
+        const response:SessionModel = await session.save()
+
+        localStorage.setItem("token", response?.token as string)
         this.router.transitionTo("/");
-      }).catch((e:any)=>{
+      }catch(e){
         alert("Something went wrong");
-        console.log(e)
-      });
-    }catch(e){
-      console.log(e)
-    }
+      }
   }
  
 }
--- app/routes/authenticated.ts
+++ app/routes/authenticated.ts
@@ -1,7 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject, service } from '@ember/service';
 
-import type Transition from '@ember/routing/transition';
 import type RouterService from '@ember/routing/router-service';
 import type SessionService from 'frontend-onboarding-template/services/session';
 
@@ -9,7 +8,7 @@ export default class AuthenticatedRoute extends Route {
     @inject('router') declare router:RouterService;
     @service declare session:SessionService;
 
-    beforeModel(transition: Transition): Promise<unknown> | void {
+    beforeModel(): Promise<unknown> | void {
         if(!this.session.isAuthenticated()){
             this.router.transitionTo("/login")
         }
