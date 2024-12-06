654d57d186a698725f1bf0e71af7f38bd6dc86a5
Filip Spanovic <filipspanovic@MacBook-Pro-od-Filip.local>
2024-12-06 13:03:16 +0100

    people model deleted

--- app/routes/authenticated/time-tracker.ts
+++ app/routes/authenticated/time-tracker.ts
@@ -1,12 +1,6 @@
 import Route from '@ember/routing/route';
-import { inject } from '@ember/service';
-
-import type StoreService from '@ember-data/store'
 
 export default class AuthenticatedTimeTrackerRoute extends Route {
-    @inject("store") declare store:StoreService;
     
-    model(){
-        return this.store.findAll("people")
-    }
+   
 }
