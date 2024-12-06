c2fe56c88f9f6dd34df29e7c43d601b65fc97af0
Filip Spanovic <filipspanovic@MacBook-Pro-od-Filip.local>
2024-12-06 09:07:10 +0100

    cleanup

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -6,15 +6,14 @@ import { pluralize } from 'ember-inflector';
 export default class ApplicationAdapter extends JSONAPIAdapter {
     namespace = "api/v2";
     host = "https://api-staging.productive.io";
-    headers = this.getHeaders
 
     pathForType(modelName) {
         return pluralize(underscore(modelName));
     }
 
-    get getHeaders(){
+    get headers(){
         return{
-            "X-Auth-token": localStorage.getItem("token"),
+            "X-Auth-token": `${localStorage.getItem("token")}`,
             "Accept": "text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5",
             "Content-Type": "application/vnd.api+json",
             "X-Organization-Id": "535",
--- app/routes/authenticated.ts
+++ app/routes/authenticated.ts
@@ -2,14 +2,13 @@ import Route from '@ember/routing/route';
 import { inject, service } from '@ember/service';
 
 import type RouterService from '@ember/routing/router-service';
-import type Transition from '@ember/routing/transition';
 import type SessionService from 'frontend-onboarding-template/services/session';
 
 export default class AuthenticatedRoute extends Route {
     @inject('router') declare router:RouterService;
     @service declare session:SessionService;
 
-    beforeModel(transition: Transition): Promise<unknown> | void {
+    beforeModel(): Promise<unknown> | void {
         if(!this.session.isAuthenticated()){
             this.router.transitionTo("/login")
         }
