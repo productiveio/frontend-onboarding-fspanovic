bf7ddb84dd1b73c80b5835ae088f01a816c26e25
Filip Spanovic <filipspanovic@MacBook-Pro-od-Filip.fritz.box>
2024-12-11 08:48:07 +0100

    cleanup

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -17,7 +17,7 @@ export default class ApplicationAdapter extends JSONAPIAdapter {
 
     get headers(){
         return{
-            "X-Auth-token": `${localStorage.getItem("token")}`,
+            "X-Auth-token": localStorage.getItem("token"),
             "Accept": "text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5",
             "Content-Type": "application/vnd.api+json",
             "X-Organization-Id": "535",
