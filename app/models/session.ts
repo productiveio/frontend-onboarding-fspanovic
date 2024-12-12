d0e35bdccfd8951298ce1187db4a83376a28c7ac
Filip Spanovic <filipspanovic@MacBook-Pro-od-Filip.local>
2024-12-12 13:09:39 +0100

    session model cleanup

--- app/models/session.ts
+++ app/models/session.ts
@@ -2,11 +2,9 @@ import Model, {attr, belongsTo} from '@ember-data/model';
 
 import type UserModel from './user';
 
-
 export default class SessionModel extends Model {
   @attr('string') token?: string;
   @attr('string') userId?: string;
-
   @attr("string") email?: string;
   @attr("string") password?: string;
 
