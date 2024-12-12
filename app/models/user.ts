e7a203b4c1c581d93a8576605a0e4f44aa2f04f2
Filip Spanovic <filipspanovic@MacBook-Pro-od-Filip.local>
2024-12-12 13:25:28 +0100

    user model cleanup

--- app/models/user.ts
+++ app/models/user.ts
@@ -3,15 +3,12 @@ import Model, {attr,hasMany, type SyncHasMany} from '@ember-data/model';
 import type OrganizationMembershipModel from './organization-membership';
 import type SessionModel from './session';
 
-
 export default class UserModel extends Model {
   @attr('string') firstName?: string;
   @attr("string") lastName?: string;
   @attr("string") email?: string;
-  
-
   @attr("number") defaultOrganizationId?: number;
 
-  @hasMany("session", {async: false, inverse: null}) sessions?:SessionModel;
+  @hasMany("session", {async: false, inverse: null}) sessions?: SessionModel;
   @hasMany('organization-membership', {async: false, inverse: 'user'}) organizationMemberships?: SyncHasMany<OrganizationMembershipModel>;
 }
