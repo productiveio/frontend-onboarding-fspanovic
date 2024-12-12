91479ddb24e5f2c1c8c98b617f2c96048f3af402
Filip Spanovic <filipspanovic@MacBook-Pro-od-Filip.local>
2024-12-12 09:11:46 +0100

    blank spaces removed

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,9 +3,7 @@ import JSONAPIAdapter from '@ember-data/adapter/json-api';
 
 import { pluralize } from 'ember-inflector';
 
-
 export default class ApplicationAdapter extends JSONAPIAdapter {
-
     namespace = "api/v2";
     host = "https://api-staging.productive.io";
 
