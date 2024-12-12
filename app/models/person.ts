b8123da4e27fa1efb7e651293ad921f9c76e4e9e
Filip Spanovic <filipspanovic@MacBook-Pro-od-Filip.local>
2024-12-12 12:58:17 +0100

    peron model cleanup

--- app/models/person.ts
+++ app/models/person.ts
@@ -2,7 +2,6 @@ import Model, {attr, hasMany, type SyncHasMany} from '@ember-data/model';
 
 import type TimeEntryModel from './time-entry';
 
-
 export default class PersonModel extends Model {
   @attr('string') firstName?: string;
   @attr('string') lastName?: string;
@@ -12,14 +11,11 @@ export default class PersonModel extends Model {
   @attr('string') colorId?: string;
   @attr('string') userId?: string;
 
-
   @attr('boolean') autotracking?: boolean;
   @attr('boolean') placeholder?: boolean;
   @attr('boolean') sampleData?: boolean;
   @attr('boolean') twoFactorAuth?: boolean;
 
-
   @hasMany('time-entry', {async: false, inverse: 'person'}) timeEntries?: SyncHasMany<TimeEntryModel>;
-
   @hasMany('person', {async: false, inverse: null, includeable: true}) customFieldPeople?: PersonModel;
 }
