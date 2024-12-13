cd38fcbea852780d1029af2ab56d582f0d67bd9b
Filip Spanovic <filipspanovic@MacBook-Pro-od-Filip.local>
2024-12-13 11:04:31 +0100

    get organization memberships moved to authenticated route

--- app/routes/application.ts
+++ app/routes/application.ts
@@ -1,32 +1,5 @@
 import Route from '@ember/routing/route';
-import {inject, service} from '@ember/service';
-
-import type Transition from '@ember/routing/transition';
-import type StoreService from '@ember-data/store'
-import type OrganizationMembershipModel from 'frontend-onboarding-template/models/organization-membership';
-import type SessionService from 'frontend-onboarding-template/services/session';
 
 export default class ApplicationRoute extends Route {
-    @inject('store') declare store: StoreService;
-    @service declare session:SessionService;
-
-  async model() {
-    const organizationMemberships = await this.store.findAll('organization-membership');
-
-    return organizationMemberships[0];
-  }
- 
-  setupController(controller, model:OrganizationMembershipModel, transition:Transition) {
-    super.setupController(controller, model, transition);
-
-    const user = model.user; 
-    const person = model.person
-
-    if(!user || !person){
-      return;
-    }
-
-    this.session.user = user;
-    this.session.person = person;
-  } 
+  
 }
--- app/routes/authenticated.ts
+++ app/routes/authenticated.ts
@@ -2,9 +2,13 @@ import Route from '@ember/routing/route';
 import {inject, service} from '@ember/service';
 
 import type RouterService from '@ember/routing/router-service';
+import type Transition from '@ember/routing/transition';
+import type StoreService from '@ember-data/store'
+import type OrganizationMembershipModel from 'frontend-onboarding-template/models/organization-membership';
 import type SessionService from 'frontend-onboarding-template/services/session';
 
 export default class AuthenticatedRoute extends Route {
+    @inject('store') declare store: StoreService;
     @inject('router') declare router:RouterService;
     @service declare session:SessionService;
 
@@ -13,4 +17,24 @@ export default class AuthenticatedRoute extends Route {
             this.router.transitionTo('/login')
         }
     }
+
+    async model() {
+        const organizationMemberships = await this.store.findAll('organization-membership');
+
+        return organizationMemberships[0];
+    }
+ 
+    setupController(controller, model:OrganizationMembershipModel, transition:Transition) {
+        super.setupController(controller, model, transition);
+
+        const user = model.user; 
+        const person = model.person
+
+        if(!user || !person){
+        return;
+        }
+
+        this.session.user = user;
+        this.session.person = person;
+    }  
 }
